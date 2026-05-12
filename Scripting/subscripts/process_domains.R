5b665d38f175a4e4a4c138667a4b73c31f47f27c
mkonigs <71269570+mkonigs@users.noreply.github.com>
2026-05-12 07:22:09 -0700

    Update process_domains.R

--- Scripting/subscripts/process_domains.R
+++ Scripting/subscripts/process_domains.R
@@ -33,14 +33,13 @@ et_exp <- read.table("../emma_toolbox_data/Databases/ET_DATABASE.txt", header = 
 
 # format subject numbers so that repeated measures have identical subject number, depending on in backup env or not
 if (exists("bu")){
-
   # depending on setting
   if(center <- "KNVB"){
     et_exp$subj <- as.numeric(substr(et_exp$subj, 1, 6))
     subj_base <- as.numeric(substr(subj, 1, 6))
   }else{
-    subj_base <- as.numeric(subj)
-    }
+    subj_base <- as.numeric(subj)}
+}
 
 # select the (repeated) measurement(s) associated with the subject
 et_exp <- et_exp[which(et_exp$subj %in% as.numeric(subj_base)),]
